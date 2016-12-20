3dd50600963d7eb426ce861b0f13238bdbc5ba0b
Xacla <area184persona@gmail.com>
2016-12-20 15:55:27 +0900

    ver1.1

--- PCA_test.py
+++ PCA_test.py
@@ -4,16 +4,35 @@ import matplotlib as plt
 import sklearn.decomposition
 
 dim=2
-data=np.loadtxt("6.7.csv",delimiter=",")
+data=np.loadtxt("/Users/tomita/Desktop/課題研究/PCA/6_7.csv",delimiter=",")
+#x,yの平均を算出し平均_*を中心とした座標に変換
+x=data[:,0]
+y=data[:,1]
+av_x=np.average(x)
+av_y=np.average(y)
+after_x = x - av_x
+after_y = y - av_y
+
+
 la=np.linalg
 pca=sklearn.decomposition.PCA(dim)
 
+#固有値、固有ベクトルを調べる
 result=pca.fit_transform(data);
-E = pca.get_covariance()
-w,v=la.eig(E)
+E_xi_eta = pca.get_covariance()
+v,u=la.eig(E_xi_eta)
 
+
+
+#データ保存
 np.savetxt("result6_7.csv", result, delimiter=",")
-np.savetxt("6_7kyousan.txt", E, delimiter=",")
-np.savetxt("6_7w.csv", w, delimiter=",")
-np.savetxt("6_7v.csv", v, delimiter=",")
-print(result);
+np.savetxt("6_7kyousan.txt", E_xi_eta, delimiter=",")
+np.savetxt("6_7u.csv", u, delimiter=",")#固有ベクトル
+np.savetxt("6_7v.csv", v, delimiter=",")#固有値
+
+#xi,etaをプロット
+xi=u[0][0]* after_x + u[1][0] * after_y
+eta=u[0][1]* after_x + u[1][1] * after_y
+
+plt.plot(xi,eta,"g")
+plt.show
